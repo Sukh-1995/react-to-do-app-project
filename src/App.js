61d2911714dec68b533717a4e891448290f8a1dd
Sukh-1995 <ersukh95@gmail.com>
2025-03-13 17:26:50 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { AuthProvider } from './auth/AuthContext';
+import Login from './auth/Login';
+import ProtetecedRoute from './auth/ProtetecedRoute';
+import Register from './auth/Register';
+import Navbar from './components/Navbar';
+import { TaskProvider } from './context/TaskContext';
+import About from './pages/About';
+import CreateTask from './pages/CreateTask';
+import Home from './pages/Home';
+import PageNotFound from './pages/PageNotFound';
+import Profile from './pages/Profile';
+import TaskList from './pages/TaskList';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Routes setup using react router dom
+    <BrowserRouter>
+      <AuthProvider>
+        <TaskProvider>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Navigate to="/login" />}></Route>
+            <Route path='/' element={<Home />}>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/register' element={<Register />}></Route>
+            </Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/task-list' element={<ProtetecedRoute><TaskList /></ProtetecedRoute>}></Route>
+            <Route path='/create-task' element={<CreateTask />}></Route>
+            <Route path='/profile' element={<Profile />}></Route>
+            <Route path='*' element={<PageNotFound />}></Route>
+          </Routes>
+        </TaskProvider>
+      </AuthProvider>
+    </BrowserRouter>
+
   );
 }
 
